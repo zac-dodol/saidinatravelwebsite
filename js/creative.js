f62a7697c59cd7cd83d3235724edfb4e2f818d7a
zac-dodol <akmalhazimothman@hotmail.com>
2025-02-07 19:18:04 +0800

    udpate layout and ws links and gallery feature update

--- js/creative.js
+++ js/creative.js
@@ -1,33 +1,41 @@
-(function($) {
-  "use strict"; // Start of use strict
+(function ($) {
+  ("use strict"); // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 57,
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 57,
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -41,35 +49,65 @@
 
   // Scroll reveal calls
   window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
+  sr.reveal(
+    ".sr-icons",
+    {
+      duration: 600,
+      scale: 0.3,
+      distance: "0px",
+    },
+    200
+  );
+  sr.reveal(".sr-button", {
     duration: 1000,
-    delay: 200
+    delay: 200,
   });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
+  sr.reveal(
+    ".sr-contact",
+    {
+      duration: 600,
+      scale: 0.3,
+      distance: "0px",
+    },
+    300
+  );
 
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
+  // Magnific popup calls for single image showing all
+  $(".popup-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: false,
+      navigateByImgClick: false,
+      preload: [0, 1],
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  });
+
+  // Magnific popup calls for specific gallery
+  // Define the common configuration for magnificPopup
+  const magnificPopupConfig = {
+    delegate: "a.popup-link", // Child items to open in the popup
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1]
+      preload: [0, 1],
     },
     image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  };
 
+  // Loop through the gallery identifiers and initialize magnificPopup
+  for (let i = 1; i <= 8; i++) {
+    $(`.image-gallery-tour${i}`).magnificPopup(magnificPopupConfig);
+    $(`.image-gallery-umrah${i}`).magnificPopup(magnificPopupConfig);
+  }
 })(jQuery); // End of use strict
